63c7e2ff30434f084452fb2e26bbd05157789fa3
Meyly-SK <meylymerma03@gmail.com>
2023-10-31 17:07:54 -0500

    Creacion Proyecto

---
+++ src/main/java/com/proyecto/ApiClinicaApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiClinicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiClinicaApplication.class, args);
+	}
+
+}
