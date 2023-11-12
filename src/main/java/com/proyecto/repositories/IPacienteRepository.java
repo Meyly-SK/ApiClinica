51a8429821fcd1161a3d4e1e880be9c39c4b31d0
unknown <i202115016@cibertec.edu.pe>
2023-11-12 15:38:06 -0500

    Reset

--- src/main/java/com/proyecto/repositories/IPacienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.proyecto.model.Paciente;
-
-public interface IPacienteRepository extends JpaRepository<Paciente, Integer> {
-
-}
--- src/main/java/com/proyecto/utils/UtilsApi.java
+++
@@ -1,6 +0,0 @@
-package com.proyecto.utils;
-
-public class UtilsApi {
-
-	public enum Role { PACIENTE, PERSONAL_MEDICO, ADMINISTRADOR }
-}
