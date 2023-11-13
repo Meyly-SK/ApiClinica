27f3986a37632954c8f3a6993c5ebaa49f8baf7f
Meyly-SK <meylymerma03@gmail.com>
2023-11-13 09:15:19 -0500

    Reset

--- src/main/java/com/proyecto/repositories/ICitaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.proyecto.model.Cita;
-
-public interface ICitaRepository extends JpaRepository<Cita, Long>{
-
-}
