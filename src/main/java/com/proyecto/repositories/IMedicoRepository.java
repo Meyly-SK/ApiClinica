6b10df8df56421ff38df8698110ae9e05ed83d72
Meyly-SK <meylymerma03@gmail.com>
2023-11-13 09:09:19 -0500

    Reseteo

--- src/main/java/com/proyecto/repositories/IMedicoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.proyecto.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.proyecto.model.Medico;
-
-public interface IMedicoRepository extends JpaRepository<Medico, Integer> {
-
-}
