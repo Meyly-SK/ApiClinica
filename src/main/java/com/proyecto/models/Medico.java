e9e971ecb02ba24afb30b6315309f394ca78294a
Meyly-SK <meylymerma03@gmail.com>
2023-11-13 12:49:32 -0500

    Estado Base

---
+++ src/main/java/com/proyecto/models/Medico.java
@@ -0,0 +1,46 @@
+package com.proyecto.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "medico")
+public class Medico implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "medico_id")
+	private int  id;
+	
+	@Column(name = "medico_especialidad")
+	private String especialidad;
+	
+	@OneToOne
+	@JoinColumn(name = "usuario_id")
+	private Usuario usuario;
+	
+	@OneToMany(mappedBy = "medico")
+    private List<Cita> citas;
+	
+	@OneToMany(mappedBy = "medicoH")
+    private List<Historial_Clinico> historiales;
+	
+}
