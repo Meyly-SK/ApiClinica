7577388ea67ef032c7261492a3f797a2197a1b5b
Meyly-SK <meylymerma03@gmail.com>
2023-11-14 11:36:03 -0500

    Implementacion Medico

--- src/main/java/com/proyecto/restcontrollers/AuthController.java
+++ src/main/java/com/proyecto/restcontrollers/AuthController.java
@@ -1,4 +1,4 @@
-package com.proyecto.controllers;
+package com.proyecto.restcontrollers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
--- src/main/java/com/proyecto/restcontrollers/PacienteController.java
+++ src/main/java/com/proyecto/restcontrollers/PacienteController.java
@@ -1,4 +1,4 @@
-package com.proyecto.controllers;
+package com.proyecto.restcontrollers;
 
 import java.util.List;
 
